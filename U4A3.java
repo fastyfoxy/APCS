157376df19885e8bbb79063648ab2280ec2e96f0
fastyfoxy <fastyfoxy@gmail.com>
2016-11-14 10:39:47 -0600

    Add files via upload

---
+++ U4A3.java
@@ -0,0 +1,16 @@
+// Tariq
+//
+// This program will create a salary schedule for teachers at any school.
+
+import java.util.StringTokenizer;
+import java.awt.Container;
+import javax.swing.JApplet;
+import javax.swing.JTextArea;
+
+public class U4A3 extends JApplet
+{
+	public void init()
+	{
+
+	}
+}
