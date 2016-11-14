8dcbaacedca70bca70bbb7c0fcca21cdea789c1a
fastyfoxy <fastyfoxy@gmail.com>
2016-11-14 12:08:59 -0600

    Update U4A3.java

--- U4A3.java
+++ U4A3.java
@@ -11,10 +11,19 @@ public class U4A3 extends JApplet
 {
 	public void init()
 	{
-    for(int c = 1, c=steps, c++)
-    {
-      int multi =((0.03*c)+1)*base;
-      out.append(c+"    "+multi;
-    }
+		String input = JOptionPane.showInputDialog("Enter Base Salary (10000-50000),\n Enter Number of Lanes(3-6),\nEnter Number of Steps per Lane(10-20),\n\n(Separate the numbers with single spaces)");
+		StringTokenizer tokenizer = new StringTokenizer(input);
+		String token1 = tokenizer.nextToken();
+		String token2 = tokenizer.nextToken();
+		String token3 = tokenizer.nextToken();
+		int base = Integer.parseInt(token1);
+		int lanes = Integer.parseInt(token2);
+		int steps = Integer.parseInt(token3);
+   		for(int c = 1, c=steps, c++)
+  		  {
+		  if(c=1) int multi = base; 
+		  else multi =((0.03*c)+1)*base;
+  		  out.append(c+"    "+multi;
+  		  }
 	}
 }
