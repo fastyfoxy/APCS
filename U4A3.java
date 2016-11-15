4412fdb7f794cff7abdb06cc825d07890f4aa7b6
fastyfoxy <fastyfoxy@gmail.com>
2016-11-15 10:29:24 -0600

    Update U4A3.java

--- U4A3.java
+++ U4A3.java
@@ -3,27 +3,45 @@
 // This program will create a salary schedule for teachers at any school.
 
 import java.util.StringTokenizer;
+import javax.swing.JOptionPane;
 import java.awt.Container;
 import javax.swing.JApplet;
+import java.awt.Font;
 import javax.swing.JTextArea;
 
 public class U4A3 extends JApplet
 {
 	public void init()
 	{
-		String input = JOptionPane.showInputDialog("Enter Base Salary (10000-50000),\n Enter Number of Lanes(3-6),\nEnter Number of Steps per Lane(10-20),\n\n(Separate the numbers with single spaces)");
+		String input = JOptionPane.showInputDialog("Enter Base Salary (10000-50000),\nEnter Number of Lanes (3-6),\nEnter Number of Steps per Lane (10-20)\n\n(Separate the numbers with single spaces)");
 		StringTokenizer tokenizer = new StringTokenizer(input);
 		String token1 = tokenizer.nextToken();
 		String token2 = tokenizer.nextToken();
 		String token3 = tokenizer.nextToken();
 		int base = Integer.parseInt(token1);
-		int lanes = Integer.parseInt(token2);
-		int steps = Integer.parseInt(token3);
-   		for(int c = 1, c=steps, c++)
-  		  {
-		  if(c=1) int multi = base; 
-		  else multi =((0.03*c)+1)*base;
-  		  out.append(c+"    "+multi;
-  		  }
+		int lane = Integer.parseInt(token2);
+		int step = Integer.parseInt(token3);
+		JTextArea out = new JTextArea();
+
+		out.append("		Salary Schedule\n\n");
+		
+		for(int c = 1; c>=lane; c++)
+		{
+			if(c<=1) out.append("      "+c);
+			else out.append("    "+c);
+		}
+		
+		out.append("\n\n");
+		
+		double multiplier = 0;
+		for(int c=1; c>=step; c++)
+		{
+			multiplier += 0.9*step;
+			if(c<=1)	out.append(c+"    "+base);
+			else out.append("  "+(base*(1+multiplier)));
+		}
+		
+		Container container = getContentPane();
+		container.add(out);
 	}
 }
