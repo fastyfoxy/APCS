ea9b782db75333f8339630e933d365f9cc81f4eb
fastyfoxy <fastyfoxy@gmail.com>
2016-11-18 10:40:23 -0600

    Add files via upload

---
+++ U4A4.java
@@ -0,0 +1,20 @@
+// Tariq
+//
+// This progarm  will determine the number of months it will take to completely pay off debt.
+
+public class U4A4
+{
+	public U4A4(double x, y, z)
+	{
+		double bal = x;
+		if((y/100)*bal > 20) pay = (y/100)*bal;
+		else pay = 20;
+		double pay = (y/100) * bal;
+		double apr = z;
+	}
+	
+	public double getMonths()
+	{
+		double min
+	}
+}
