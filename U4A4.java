0dbc2ae2e089a5d3b3aa22017a6553376a1eddce
fastyfoxy <fastyfoxy@gmail.com>
2016-11-18 12:09:06 -0600

    Update U4A4.java

--- U4A4.java
+++ U4A4.java
@@ -2,19 +2,40 @@
 //
 // This progarm  will determine the number of months it will take to completely pay off debt.
 
+import java.util.Scanner;
+
 public class U4A4
 {
+	private double balance, apr, total, minimumPay;
+	private int months, percent;
+	
+	public static void main(String[] args)
+	{
+		U4A4 debt = new U4A4();	
+	}
 	public U4A4(double x, y, z)
 	{
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter credit card balance: ");
+		balance = in.nextDouble();
+		
+		System.out.print("\n\nEnter minimum monthly payment (as a % of the balance): "
+		percent = in.nextInt();
+				 
+		System.out.print("\n\nEnter the annual percentage rate: ");
+		percent = in.nextInt();
+				 
 		double bal = x;
-		if((y/100)*bal > 20) pay = (y/100)*bal;
-		else pay = 20;
-		double pay = (y/100) * bal;
+		if((percent/100.0)*balance > 20.0) minimumPay = (percent/100.0)*balance;
+		else if (bal<20.0) pay = bal;
+		else pay = 20.0;
+		double pay = (y/100.0) * bal;
 		double apr = z;
 	}
 	
-	public double getMonths()
+	public int getMonths()
 	{
-		double min
+		
 	}
 }
