e52fc509241cbd51d6e57e740caa6a2c49c4fd1d
fastyfoxy <fastyfoxy@gmail.com>
2016-11-20 23:56:38 -0600

    Update U4A4.java

--- U4A4.java
+++ U4A4.java
@@ -8,31 +8,32 @@ public class U4A4
 {
 	private double balance, apr, profit, total, minimumPay;
 	private int months, percent;
-	
+
 	public static void main(String[] args)
 	{
-		U4A4 debt = new U4A4();	
+		U4A4 debt = new U4A4();
 	}
-	public U4A4(double x, y, z)
+	public U4A4()
 	{
 		Scanner in = new Scanner(System.in);
-		
+
 		System.out.print("Enter credit card balance: ");
 		balance = in.nextDouble();
-		
-		System.out.print("\n\nEnter minimum monthly payment (as a % of the balance): ");
+
+		System.out.print("\nEnter minimum monthly payment (as a % of the balance): ");
 		percent = in.nextInt();
-				 
-		System.out.print("\n\nEnter the annual percentage rate: ");
+
+		System.out.print("\nEnter the annual percentage rate: ");
 		apr = in.nextDouble();
-				
+
 		if((percent/100D)*balance > 20.0) minimumPay = (percent/100D)*balance;
 		else if (balance<20.0) minimumPay = balance;
 		else minimumPay = 20.0;
-		
-		System.out.println("\n\n\n ");
+
+		System.out.println("\n\nNumber of months to pay off debt: "+months);
+		System.out.println("\nProfit for the credit company = "+profit+"\n"); //PROFIT HAS TO BE STRING.FORMAT'ED
 	}
-	
+
 	public int getMonths()
 	{
 		profit = total - balance;
